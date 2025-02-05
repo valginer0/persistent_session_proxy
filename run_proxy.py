e1eed95fafe5c9e2b2f4ee1ac7cf8a7cfea614f8
Val Giner <valginer0@gmail.com>
2025-02-04 22:00:08 -0600

    refactor: simplify proxy server shutdown
    - Remove complex threading and file-based stopping mechanism
    - Return to simple Ctrl+C handling
    - Clean up signal handlers
    - Improve shutdown messages

--- run_proxy.py
+++ run_proxy.py
@@ -1,4 +1,4 @@
-"""Run the persistent session proxy directly."""
+"""Run the proxy server."""
 import sys
 from pathlib import Path
 
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser(description='Start the persistent session proxy server')
-    parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
+    parser.add_argument('--host', default='127.0.0.1', help='Host to bind to')
     parser.add_argument('--port', type=int, default=8080, help='Port to listen on')
     
     args = parser.parse_args()
